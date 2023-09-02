fc675780bf46a66fd5f5fed4bf108f1f14a4e20b
oyoha <vasal30000@mail.ru>
2023-09-02 20:01:10 +0800

    first commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-const user = "Alex";
-alert(`Hello ${user}`);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
+
+const a = prompt("Один из последних фильмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних фильмов?", ""),
+  d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
